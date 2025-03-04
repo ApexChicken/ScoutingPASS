549932ae40e3cf635624bec524959617a0ee9d2f
Apex Chicken <129459409+ApexChicken@users.noreply.github.com>
2025-03-04 15:31:29 -0500

    Update reefscape_pit_scouting.js
    I just input the thing into jsonlint and pasted the whole thing back in. SURELYYYY this wont cause any more problems right? RIGHT?

--- 2025/reefscape_pit_scouting.js
+++ 2025/reefscape_pit_scouting.js
@@ -1,94 +1,102 @@
 var config_data = `
 {
-  "dataFormat": "tsv",
-  "title": "4564 SCOUTING 2025",
-  "page_title": "REEFSCAPE",
-  "pitConfig": "true",
-  "prematch": [
-    { "name": "Team Number",
-      "code": "t",
-      "type": "number"
-    },
-    { "name": "Dimensions",
-      "code": "di",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    },
-    { "name": "Weight",
-      "code": "wei",
-      "type": "number",
-      "defaultValue": ""
-    },
-    { "name": "Drivetrain",
-      "code": "drv",
-      "type": "radio",
-      "choices": {
-        "s": "Swerve<br>",
-        "w": "West Coast/Tank<br>",
-        "o": "Other"
-      },
-      "defaultValue": "o"
-    },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "Drivetrain Motor",
-      "code": "mot",
-      "type": "radio",
-      "choices": {
-        "n": "Neo<br>",
-        "f": "Falcon<br>",
-        "c": "CIM<br>",
-        "x": "Other<br>"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "Pickup Coral",
-      "code": "pc",
-      "type": "bool"
-    },
-    { "name": "Floor pickup Coral",
-      "code": "fpu",
-      "type": "bool"
-    },
-    { "name": "Pickup Algae",
-      "code": "pa",
-      "type": "bool"
-    },
-    { "name": "Floor pickup Algae",
-      "code": "fpa",
-      "type": "bool"
-    },
-    { "name": "Autos",
-      "code": "aut",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    },
-    { "name": "Comments",
-      "code": "co",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    }
-  ],
-  "auton": [
-  ],
-  "teleop": [
-  ],
-  "endgame": [
-  ],
-  "postmatch": [
-  ]
+    "dataFormat": "tsv",
+    "title": "4564 SCOUTING 2025",
+    "page_title": "REEFSCAPE",
+    "pitConfig": "true",
+    "prematch": [
+        {
+            "name": "Team Number",
+            "code": "t",
+            "type": "number"
+        },
+        {
+            "name": "Dimensions",
+            "code": "di",
+            "type": "text",
+            "size": 20,
+            "maxSize": 250
+        },
+        {
+            "name": "Weight",
+            "code": "wei",
+            "type": "number",
+            "defaultValue": ""
+        },
+        {
+            "name": "Drivetrain",
+            "code": "drv",
+            "type": "radio",
+            "choices": {
+                "s": "Swerve<br>",
+                "w": "West Coast/Tank<br>",
+                "o": "Other"
+            },
+            "defaultValue": "o"
+        },
+        {
+            "name": "Swerve Ratio",
+            "code": "sr",
+            "type": "radio",
+            "choices": {
+                "1": "L1 (8.14:1)<br>",
+                "2": "L2 (6.75:1)<br>",
+                "3": "L3 (6.12:1)<br>",
+                "4": "L4 (5.14:1)<br>",
+                "o": "Other ratio (put in comments)<br>",
+                "x": "Not Swerve"
+            },
+            "defaultValue": "x"
+        },
+        {
+            "name": "Drivetrain Motor",
+            "code": "mot",
+            "type": "radio",
+            "choices": {
+                "n": "Neo<br>",
+                "f": "Falcon<br>",
+                "c": "CIM<br>",
+                "x": "Other<br>"
+            },
+            "defaultValue": "x"
+        },
+        {
+            "name": "Pickup Coral",
+            "code": "pc",
+            "type": "bool"
+        },
+        {
+            "name": "Floor pickup Coral",
+            "code": "fpu",
+            "type": "bool"
+        },
+        {
+            "name": "Pickup Algae",
+            "code": "pa",
+            "type": "bool"
+        },
+        {
+            "name": "Floor pickup Algae",
+            "code": "fpa",
+            "type": "bool"
+        },
+        {
+            "name": "Autos",
+            "code": "aut",
+            "type": "text",
+            "size": 20,
+            "maxSize": 250
+        },
+        {
+            "name": "Comments",
+            "code": "co",
+            "type": "text",
+            "size": 20,
+            "maxSize": 250
+        }
+    ],
+    "auton": [],
+    "teleop": [],
+    "endgame": [],
+    "postmatch": []
 }`;
